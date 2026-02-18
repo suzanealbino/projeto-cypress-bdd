e52bb7150fc54e304740934251d8f882c3000cf2
Suzane Albino <119638079+suzanealbino@users.noreply.github.com>
2026-02-17 21:21:38 -0300

    Update Cypress configuration to include plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
-// ... (mantenha os imports do topo)
+
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.feature",
